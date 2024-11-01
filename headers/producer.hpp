d3467be2285b8ebef2e7f51881955f3a3597ac49
A-I-Hall <ahall142@kent.edu>
2024-11-01 13:23:35 -0400

    Create producer.hpp

---
+++ headers/producer.hpp
@@ -0,0 +1,22 @@
+/**
+ * @file consumer.hpp
+ * @author Annika Hall
+ * @brief The consumer header for the producer/consumer problem
+ * @date 2024-11-01
+ * 
+ */
+
+
+#ifndef CONSUMER_HPP
+#define CONSUMER_HPP
+
+#include <stdio.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <iostream>
+#include <fcntl.h>
+#include <sys/mman.h>
+
+int main(int, char **);
+
+#endif
